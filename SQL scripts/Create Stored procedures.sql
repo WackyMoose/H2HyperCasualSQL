a5096e0344129a2284ae2c1304cc4bfbe0b81aeb
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2021-12-17 11:24:33 +0100

    changed GetUserById stored procedure

--- SQL scripts/Create Stored procedures.sql
+++ SQL scripts/Create Stored procedures.sql
@@ -18,16 +18,13 @@ as
 	order by KDR desc
 go;
 
-create procedure GetPlayerById @Id int
+alter procedure GetPlayerById @Id int
 as
-	SELECT p.*, u.*, m.*, ms.* 
+	SELECT p.*, u.*
 	FROM [Tanx].[dbo].[Players] AS p
-	INNER JOIN [Tanx].[dbo].[PlayerMatches] as pm ON pm.PlayerId = p.Id
-	INNER JOIN [Tanx].[dbo].[Matches] as m ON m.Id = pm.MatchId
 	INNER JOIN [Tanx].[dbo].[Users] AS u ON u.PlayerId = p.Id
-	INNER JOIN [Tanx].[dbo].[MatchStatus] as ms ON ms.Id = m.MatchStatusId
 	WHERE p.Id = @Id;
-go;
+go
 
 create procedure GetPlayerByIdMinimal @Id int
 as
